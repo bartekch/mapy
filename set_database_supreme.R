7732e65789b1307059448a8b183e893770f804d9
bchrol <bartek.chrol@gmail.com>
2015-05-18 16:46:21 +0200

    create new database for supreme courts

---
+++ set_database_supreme.R
@@ -0,0 +1,23 @@
+library("dplyr")
+library("saos")
+library("RSQLite")
+
+data(scchambers)
+supreme <- readRDS("data//data_supreme.RDS")
+
+date <- extract(supreme, "judgmentDate")
+division <- extract(supreme, "division")
+personel <- extract(supreme, "personnelType")
+form <- extract(supreme, "form")
+type <- extract(supreme, "judgmentType")
+
+
+date %>%
+  inner_join(division) %>%
+  inner_join(personel) %>%
+  inner_join(form) %>%
+  inner_join(type) -> judgments
+
+con <- dbConnect(RSQLite::SQLite(), "data/supreme_court.db")
+
+dbWriteTable(con, "judgments", judgments, row.names = FALSE)
